fbfc04c9c4a88e20a09c7e96c06a2d5bfd5296bf
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-09-15 15:32:47 +0200

    Update background.js

--- background.js
+++ background.js
@@ -13,3 +13,25 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
+
+  if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    chrome.scripting.insertCSS({
+      target: {tabId: tabId},
+      files: ["foreground_styles.css"]
+    })
+        .then(() => {
+          chrome.scripting.executeScript({
+            target: {tabId: tabId},
+            files: ["foreground.js"]
+          })
+              .then(() => {
+                console.log("INJECTED");
+              })
+
+
+        })
+        .catch(err => console.log(err));
+  }
+});
