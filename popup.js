51f5db1089aabf545cbe3a783b5cecf1d31a25c7
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-09-16 16:08:27 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,66 +1,29 @@
 myFunction = function () {
 
     let twitterState = ["healthy", "careful", "read the newspaper"]
+    let buttonColours = ["#ea440d", "#39ea0d", "#cb1032"]
     const ws = new WebSocket('ws://localhost:8080');
-    const trackWord = "obama"
+    const trackWord = "covid"
+    const r = document.querySelector(':root');
 
-    // ws.addEventListener("open", (ev) => {
-    //     ws.send("obama".toString());
-    //     console.log("hi")
-    //
-    // })
 
     ws.onopen = function (eve)  {
         console.log("Connected");
         ws.send(trackWord.toString())
     };
 
-    // ws.send(trackWord.toString())
     console.log("data sent")
 
     ws.onmessage = (evt) => {
 
         const data = evt.data.toString();
-        // const action = data.action;
-        // const args = data.args;
-        // console.log(data);
-        // console.log(typeof (data));
-        // console.log(action);
-        // console.log(args);
 
-        // // re-create(?) blob data from websocket
-        // const blob = new Blob([evt.data],
-        //     {type: "text/plain"});
         const image = document.getElementById("div1");
         image.innerText = twitterState[parseInt(data) - 1]
-        //
-        // let reader = new FileReader();
-        // reader.readAsDataURL(blob); // converts the blob to base64 and calls onload
-        //
-        // reader.onload = function() {
-        // image.innerText = reader.result.; // data url
-        // link.click();
-        // };
-
+        r.style.setProperty("--second-color", buttonColours[parseInt(data) - 1])
 
         console.log(data)
-        //
-        // const url = URL.createObjectURL(blob);
-        //
 
-        // const img = new Image(0, 0);
-        // //
-        // img.onload = () => {
-        //
-        //     // get canvas context
-        //     let ctx = image.getContext("2d");
-        //
-        //     ctx.clearRect(0, 0, image.width, image.height);
-        //     ctx.drawImage(img, 0, 0);
-        //
-        // };
-
-        // image.innerText = url;
 
 
     };
