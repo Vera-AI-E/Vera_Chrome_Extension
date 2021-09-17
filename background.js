8bea7f9913704dc1dfd9a7f6522fd043916fc9fc
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-09-17 22:16:07 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
-
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+        chrome.scripting.insertCSS({
+            target: { tabId: tabId},
+            files: ["foreground.css"]
+        })
+            .then(() => {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabId},
+                    files: ["foreground.js"]
+                })
+                    .then(() => {
+                        console.log("FOREGROUND INJECTED")
+                    })
+        })
+            .catch(err => console.log(err))
+    }
+})
