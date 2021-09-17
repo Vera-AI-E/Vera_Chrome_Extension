39fd3c5a1d1aa39b238563a60eb748bcdbfe4bb2
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-09-17 22:16:41 +0200

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,2 +1,9 @@
+const mainContainer = document.createElement("DIV");
+const indicatorBlob = document.createElement("DIV");
 
+mainContainer.classList.add("mainContainer");
+indicatorBlob.id = "indicatorBlob";
 
+mainContainer.appendChild(indicatorBlob);
+
+document.querySelector("body").appendChild(mainContainer)
