55d3b4840487891c24b43655f775e50e15c9a54f
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-09-19 19:05:18 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,32 +1,40 @@
 myFunction = function () {
+    console.log("here")
+    const btn = document.getElementById("button");
+    btn.classList.add("button--loading")
 
-    let twitterState = ["healthy", "careful", "read the newspaper"]
-    let buttonColours = ["green", "orange", "red"]
+    let twitterState = ["healthy", "careful", "read the newspaper"];
+    let buttonColours = ["green", "orange", "red"];
     const ws = new WebSocket('ws://localhost:8080');
-    const trackWord = "covid"
+    const trackWord = "covid";
     const r = document.querySelector(':root');
 
 
     ws.onopen = function (eve)  {
         console.log("Connected");
-        ws.send(trackWord.toString())
+        ws.send(trackWord.toString());
     };
 
-    console.log("data sent")
+    console.log("data sent");
 
     ws.onmessage = (evt) => {
 
         const data = evt.data.toString();
+        if (data.length === 1){
+            btn.classList.remove("button--loading");
+        }
 
-        const image = document.getElementById("div1");
-        image.innerText = twitterState[parseInt(data) - 1]
-        r.style.setProperty("--first-color", buttonColours[parseInt(data) - 1])
+        const displayText = document.getElementById("div1");
+        displayText.innerText = twitterState[parseInt(data) - 1];
+        r.style.setProperty("--first-color", buttonColours[parseInt(data) - 1]);
 
-        console.log(data)
+        console.log(data);
 
 
 
     };
 
 }
-document.getElementById("button").addEventListener("click", myFunction)
+
+
+document.getElementById("button").addEventListener("click", myFunction);
